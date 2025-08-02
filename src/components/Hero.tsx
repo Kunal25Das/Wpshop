e1b74841b947719935841cbaf7f8e2bec7985bef
AnuvabMaity <anuvabmaity99@gmail.com>
2025-08-02 23:24:31 +0530

    Fixed Page Responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
 
   return (
     <section className="relative min-h-screen flex items-center justify-center text-white text-center">
-      <div className="relative z-10 p-4">
-        <h1 className="text-4xl md:text-5xl font-bold mb-4 font-bruno-ace">
+      <div className="relative z-10 p-4 overflow-x-hidden">
+        <h1 className="text-3xl md:text-5xl font-bold mb-4 font-bruno-ace text-pretty">
           WordPress Community Kolkata <br/> X <br/> G.C.E.T.T.S
         </h1>
-        <p className="text-lg md:text-2xl mb-8 font-audiowide">At G.C.E.T.T.S Auditorium. Spread the Word.</p>
+        <p className="text-lg md:text-2xl mb-8 font-audiowide">Coming Soon at G.C.E.T.T.S. <br/> #SpreadTheWord.</p>
         <CountdownTimer targetDate={targetDate} />
         <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-full transition duration-300">
           Register Now
