0bc77b36a241252f4da21278f805e642f0941596
AnuvabMaity <anuvabmaity99@gmail.com>
2025-08-03 19:19:18 +0530

    Final Changes (hopefully)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 const About = () => {
   return (
-    <section className="py-20 px-40 text-white">
+    <section className="py-20 px-10 text-white">
       <div className="container mx-auto px-4">
-        <div className="flex gap-20 items-center">
+        <div className="md:flex gap-10 items-center">
           <div>
             <h2 className="text-3xl font-bold mb-4">About the Community</h2>
-            <p className="text-lg text-justify w-5/6">
+            <p className="text-lg text-justify mb-5">
               The WordPress Kolkata community is a group of passionate developers, designers, and users who love WordPress. This is the first initiative to have regular Kolkata based WordPress Meetups. It is an idea to create a regular WordPress Meetup in Kolkata. There are many different WordPress users in Kolkata and it is time to connect and share ideas and knowledge and build a local WordPress community. Join us to learn, share, and connect with fellow enthusiasts.
             </p>
           </div>
